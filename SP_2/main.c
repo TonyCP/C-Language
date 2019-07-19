d9ab59d310f6aeaca86b176746b3a36f6d5f8610
_TCHNX <45986825+TonyCP@users.noreply.github.com>
2019-07-19 18:04:20 -0400

    Update main.c

--- SP_2/main.c
+++ SP_2/main.c
@@ -10,13 +10,16 @@ int main() {
   int i;
 
   // Requests user input at commmand line //
-  printf("\nPlease enter the character(s) of the alphabet in string format to check for vowels or constants: ");
+  printf("\nPlease enter the character(s) of the alphabet in string format 
+         to check for vowels or constants: ");
   scanf("%s", alpha);
 
   // Loops the length of character input //
   for(i = 0; i < strlen(alpha); i++){
     // Checks for vowels (a, e, i, o, u) but not (y) //
-    if(alpha[i]=='a' || alpha[i]=='A' || alpha[i]=='e' || alpha[i]=='E' || alpha[i]=='i' || alpha[i]=='I' || alpha[i]=='o' || alpha[i]=='O' || alpha[i]=='u' || alpha[i]=='U'){
+    if(alpha[i]=='a' || alpha[i]=='A' || alpha[i]=='e' || alpha[i]=='E' || 
+       alpha[i]=='i' || alpha[i]=='I' || alpha[i]=='o' || alpha[i]=='O' || 
+       alpha[i]=='u' || alpha[i]=='U'){
 
       // Informs user of vowels found within input //
       printf("\nVowel Found: %c\n", alpha[i]);
